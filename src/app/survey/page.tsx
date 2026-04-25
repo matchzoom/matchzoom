410baa345876257f98387a109c6a5c19cc8e0e7f
최우석 <nick501@naver.com>
2026-04-25 21:15:55 +0900

    refactor: survey 페이지 SurveyContent 컴포넌트 분리 #161

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,45 +1,5 @@
-'use client';
-
 import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { SurveyForm } from '@/features/survey/ui/SurveyForm';
-import { useSurveyForm } from '@/features/survey/hooks/useSurveyForm';
-import { usePreventNavigation } from '@/features/survey/hooks/usePreventNavigation';
-
-function SurveyContent() {
-  const searchParams = useSearchParams();
-  const mode = searchParams.get('mode') === 'edit' ? 'edit' : 'create';
-  const form = useSurveyForm(mode);
-  const { isBlocking, onStay, onLeave } = usePreventNavigation(form.isDirty);
-
-  return (
-    <SurveyForm
-      mode={form.mode}
-      step={form.step}
-      register={form.register}
-      errors={form.errors}
-      isSubmitting={form.isSubmitting}
-      isMatching={form.isMatching}
-      isMatchError={form.isMatchError}
-      isComplete={form.isComplete}
-      isBlocking={isBlocking}
-      sidoList={form.sidoList}
-      watchedPrimaryRegion={form.watchedPrimaryRegion}
-      watchedDisabilityType={form.watchedDisabilityType}
-      watchedHopeActivities={form.watchedHopeActivities}
-      onPrimaryRegionChange={form.onPrimaryRegionChange}
-      onDisabilityTypeChange={form.onDisabilityTypeChange}
-      onHopeActivitiesChange={form.onHopeActivitiesChange}
-      onNextStep={form.onNextStep}
-      onPrevStep={form.onPrevStep}
-      onSubmit={form.onSubmit}
-      onCompleteConfirm={form.onCompleteConfirm}
-      onMatchErrorClose={form.onMatchErrorClose}
-      onStay={onStay}
-      onLeave={onLeave}
-    />
-  );
-}
+import { SurveyContent } from '@/features/survey/ui/SurveyContent';
 
 export default function SurveyPage() {
   return (
---
+++ src/features/survey/ui/SurveyContent.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { SurveyForm } from './SurveyForm';
+import { useSurveyForm } from '../hooks/useSurveyForm';
+import { usePreventNavigation } from '../hooks/usePreventNavigation';
+
+export function SurveyContent() {
+  const searchParams = useSearchParams();
+  const mode = searchParams.get('mode') === 'edit' ? 'edit' : 'create';
+  const form = useSurveyForm(mode);
+  const { isBlocking, onStay, onLeave } = usePreventNavigation(form.isDirty);
+
+  return (
+    <SurveyForm
+      mode={form.mode}
+      step={form.step}
+      register={form.register}
+      errors={form.errors}
+      isSubmitting={form.isSubmitting}
+      isMatching={form.isMatching}
+      isMatchError={form.isMatchError}
+      isComplete={form.isComplete}
+      isBlocking={isBlocking}
+      sidoList={form.sidoList}
+      watchedPrimaryRegion={form.watchedPrimaryRegion}
+      watchedDisabilityType={form.watchedDisabilityType}
+      watchedHopeActivities={form.watchedHopeActivities}
+      onPrimaryRegionChange={form.onPrimaryRegionChange}
+      onDisabilityTypeChange={form.onDisabilityTypeChange}
+      onHopeActivitiesChange={form.onHopeActivitiesChange}
+      onNextStep={form.onNextStep}
+      onPrevStep={form.onPrevStep}
+      onSubmit={form.onSubmit}
+      onCompleteConfirm={form.onCompleteConfirm}
+      onMatchErrorClose={form.onMatchErrorClose}
+      onStay={onStay}
+      onLeave={onLeave}
+    />
+  );
+}
